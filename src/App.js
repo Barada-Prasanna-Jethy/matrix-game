c4fa3ab084a1f2850b087cdb1b5dd56e4942206b
Barada Prasanna Jethy <59828950+Barada-Prasanna-Jethy@users.noreply.github.com>
2021-12-15 12:14:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import "./App.css";
+import Box from "./components/box";
+export default class App extends Component {
+  state = {
+    redBoxes: [],
+  };
+  changeColor(i) {
+    var newBoxes = this.state.redBoxes;
+
+    if (this.state.redBoxes.length < 2) {
+      newBoxes.push(i);
+    } else {
+      newBoxes.push(i);
+      newBoxes.shift();
+    }
+
+    this.setState({ redBoxes: newBoxes });
+  }
+  render() {
+    const boxes = [];
+    const redBoxes = this.state.redBoxes;
+    for (let i = 0; i < 16; i++) {
+      if (redBoxes.includes(i)) boxes.push(<Box key={i} color="red" />);
+      else
+        boxes.push(
+          <Box
+            key={i}
+            index={i}
+            colorChange={(index) => {
+              this.changeColor(index);
+            }}
+            color="blue"
+          />
+        );
+    }
+    return <div className="container">{boxes}</div>;
+  }
+}
